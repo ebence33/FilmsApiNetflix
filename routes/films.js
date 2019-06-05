fb4a75673eb0a6fc15226e838e7115e2d91eae7a
Bence <elo.bence33@gmail.com>
2019-06-05 10:35:43 +0200

    05-06-2019

--- routes/films.js
+++ routes/films.js
@@ -3,15 +3,15 @@ var router = express.Router();
 var model = require('../models/index');
 /* GET home page. */
 router.post('/film', function(req, res, next) {
-console.log("************************", model);
+	console.log("************************", model);
 
-model.Films.create({
-	title : req.body.title,
-	description: req.body.description,
-	image : req.body.image,
-	url : req.body.url
+	model.Films.create({
+		title : req.body.title,
+		description: req.body.description,
+		image : req.body.image,
+		url : req.body.url
 
-})
+	})
 	.then(films => res.status(201).json ({
 		error : false,
 		data: films,
@@ -27,9 +27,9 @@ model.Films.create({
 
 
 router.get('/films', function(req, res, next) {
-console.log("************************", model);
+	console.log("************************", model);
 
-model.Films.findAll({})
+	model.Films.findAll({})
 	.then(films => res.json ({
 		error : false,
 		data: films
@@ -45,10 +45,54 @@ model.Films.findAll({})
 
 
 router.put('/film/:id', function(req, res, next) {
+	console.log("************************", model);
+
+	model.Films.update({
+		title : req.body.title,
+		description: req.body.description,
+		image : req.body.image,
+		url : req.body.url
+
+	},
+	{
+	where : {
+		id: req.params.id
+	}
+})
+
+	.then(films => res.json ({
+		error : false,
+		data: films,
+		message: 'le  film a été modifié'
+	}))
+	.catch(error => res.json({
+		error:true,
+		data : [],
+		error : error
+	}));
+	console.log("************************", model);
+
 
 });
+
+
 router.delete('/film/:id', function(req, res, next) {
 
+	model.Films.destroy({
+		where: {
+			id: req.params.id
+		}
+	})
+	.then(films => res.json ({
+		error : false,
+		data: films,
+		message: 'le  film a été supprimé'
+	}))
+	.catch(error => res.json({
+		error:true,
+		data : [],
+		error : error
+	}));
 });
 
-module.exports = router;
+	module.exports = router;
